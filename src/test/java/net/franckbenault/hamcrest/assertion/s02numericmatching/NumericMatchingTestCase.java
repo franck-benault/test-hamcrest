851c71a1241188bc5433a6ae9d63f23fb4e81226
franck-benault <franck-benault@orange.fr>
2015-07-11 00:02:22 +0200

    first matching with numeric

--- src/test/java/net/franckbenault/hamcrest/assertion/s02numericmatching/NumericMatchingTestCase.java
+++ src/test/java/net/franckbenault/hamcrest/assertion/s02numericmatching/NumericMatchingTestCase.java
@@ -2,11 +2,11 @@ package net.franckbenault.hamcrest.assertion.s02numericmatching;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.nullValue;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.sameInstance;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+
 
 
 import org.junit.Test;
@@ -17,16 +17,27 @@ public class NumericMatchingTestCase {
 
 
 	@Test
-	public void t01AssertArrayEqualsTest() {
+	public void t01aAssertGreaterThan() {
 		
-		int[] ints1 = {1,3};
-		int[] ints2 = {1,3};
+		int v1 =7;
+		int v2 =5;
 
 		
-		assertThat(ints1,is(equalTo(ints2)));
+		assertThat(v1,is(greaterThan(v2)));
 	}
 
 
+	@Test
+	public void t01bAssertGreaterThanOrEqualTo() {
+		
+		int v1 =7;
+		int v2 =5;
+		int v3 =7;
+
+		
+		assertThat(v1,is(greaterThanOrEqualTo(v2)));
+		assertThat(v1,is(greaterThanOrEqualTo(v3)));
+	}
 	
 
 }
