d04538e59667e8edf598c9b698a6e8d2aae2b865
franck-benault <franck-benault@orange.fr>
2015-07-11 23:55:45 +0200

    add string matching equals

--- src/test/java/net/franckbenault/hamcrest/assertion/s03stringmatching/StringMatchingTestCase.java
+++ src/test/java/net/franckbenault/hamcrest/assertion/s03stringmatching/StringMatchingTestCase.java
@@ -3,7 +3,8 @@ package net.franckbenault.hamcrest.assertion.s03stringmatching;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.text.IsEmptyString.isEmptyString;
 import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
-
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.hamcrest.text.IsEqualIgnoringWhiteSpace.equalToIgnoringWhiteSpace;
 
 import org.junit.Test;
 
@@ -27,4 +28,24 @@ public class StringMatchingTestCase {
 		assertThat(s2,isEmptyOrNullString());
 	}
 
+	
+	@Test
+	public void t02aIsEqualIgnoringCase() {
+		
+		String s1 = "s";
+		String s2 = "S";
+	
+		assertThat(s1,equalToIgnoringCase(s2));
+	}
+
+	@Test
+	public void t02bIsEqualIgnoringWhiteSpace() {
+		
+		String s1 = "string";
+		String s2 = " string ";
+	
+		assertThat(s1,equalToIgnoringWhiteSpace(s2));
+	}
+	
+	
 }
