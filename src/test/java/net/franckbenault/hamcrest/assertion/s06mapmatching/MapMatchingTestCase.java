9deb9ad0dddf3dfea6614161d4852c6df2e0777b
franck-benault <franck-benault@orange.fr>
2015-07-27 22:29:09 +0200

    add map matching

--- src/test/java/net/franckbenault/hamcrest/assertion/s06mapmatching/MapMatchingTestCase.java
+++ src/test/java/net/franckbenault/hamcrest/assertion/s06mapmatching/MapMatchingTestCase.java
@@ -1,4 +1,4 @@
-package net.franckbenault.hamcrest.assertion.s06logicalmatching;
+package net.franckbenault.hamcrest.assertion.s06mapmatching;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.AnyOf.anyOf;
@@ -7,7 +7,7 @@ import java.util.Set;
 
 import org.junit.Test;
 
-public class LogicalMatchingTestCase {
+public class MapMatchingTestCase {
 
 	@Test
 	public void t01foo() {
