728b25ddec983f1e18080fa51f93146e0773a9eb
franck-benault <franck-benault@orange.fr>
2015-07-30 20:19:18 +0200

    add bean matching

---
+++ src/test/java/net/franckbenault/hamcrest/assertion/s07beanmatching/BeanMatchingTestCase.java
@@ -0,0 +1,17 @@
+package net.franckbenault.hamcrest.assertion.s07beanmatching;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.AnyOf.anyOf;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class BeanMatchingTestCase {
+
+	@Test
+	public void t01ahasProperty() {
+		
+
+	}
+}
